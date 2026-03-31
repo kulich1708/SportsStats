0f498eadae6f207a319ce9bd6883834664e40170
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-03-31 16:25:34 +0300

    feat(api): add Swagger configuration (#12)

--- SportsStats.API/Program.cs
+++ SportsStats.API/Program.cs
@@ -1,23 +1,79 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi;
+using System.Reflection;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace SportsStats.API
 {
-	app.MapOpenApi();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			ConfigureServices(builder);
+
+			var app = builder.Build();
+
+			ConfigureMiddleware(app);
+
+			app.Run();
+		}
+
+		private static void ConfigureServices(WebApplicationBuilder builder)
+		{
+			var services = builder.Services;
+
+			services.AddControllers();
+			services.AddEndpointsApiExplorer();
+
+			var version = Assembly.GetExecutingAssembly()
+								  .GetName()
+								  .Version?
+								  .ToString() ?? "v1";
+
+			services.AddSwaggerGen(options =>
+			{
+				options.SwaggerDoc(version, new OpenApiInfo
+				{
+					Title = "Sports Stats API",
+					Version = version,
+					Description = "API для работы со спортивной статистикой"
+				});
+
+				// XML комментарии
+				var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+				var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+				if (File.Exists(xmlPath))
+				{
+					options.IncludeXmlComments(xmlPath);
+				}
+			});
+		}
+
+		private static void ConfigureMiddleware(WebApplication app)
+		{
+			if (app.Environment.IsDevelopment())
+			{
+				var version = Assembly.GetExecutingAssembly()
+									  .GetName()
+									  .Version?
+									  .ToString() ?? "v1";
+
+				app.UseSwagger();
+
+				app.UseSwaggerUI(options =>
+				{
+					options.SwaggerEndpoint(
+						$"/swagger/{version}/swagger.json",
+						$"Sports Stats API {version}");
+
+					options.RoutePrefix = string.Empty;
+				});
+			}
+
+			app.UseHttpsRedirection();
+			app.UseAuthorization();
+			app.MapControllers();
+		}
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
