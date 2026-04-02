aed40630acfe90299263d68eeea4ea760e25277b
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-02 11:22:26 +0300

    feat(domain): add BaseEntity abstract class

---
+++ SportsStats.Domain/Common/BaseEntity.cs
@@ -0,0 +1,72 @@
+namespace Domain.Common
+{
+	public abstract class BaseEntity : IEntity
+	{
+		private int? _requestedHashCode;
+		private int _id;
+
+		// Основной идентификатор
+		public virtual int Id
+		{
+			get => _id;
+			protected set => _id = value;
+		}
+
+		// Конструктор для создания новой сущности (без Id)
+		protected BaseEntity()
+		{
+		}
+
+		// Конструктор с Id (для загрузки из БД)
+		protected BaseEntity(int id)
+		{
+			_id = id;
+		}
+
+		// Сравнение сущностей по Id и типу
+		public override bool Equals(object? obj)
+		{
+			if (obj == null || !(obj is BaseEntity other))
+				return false;
+
+			if (ReferenceEquals(this, other))
+				return true;
+
+			if (GetType() != other.GetType())
+				return false;
+
+			if (Id == 0 || other.Id == 0)
+				return false;
+
+			return Id == other.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			if (!_requestedHashCode.HasValue)
+				_requestedHashCode = (GetType().GetHashCode() * 907) + Id.GetHashCode();
+
+			return _requestedHashCode.Value;
+		}
+
+		// Для сравнения (== и !=)
+		public static bool operator ==(BaseEntity? left, BaseEntity? right)
+		{
+			if (Equals(left, null))
+				return Equals(right, null);
+
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(BaseEntity? left, BaseEntity? right)
+		{
+			return !(left == right);
+		}
+
+		// Проверка, что сущность сохранена (Id > 0)
+		public bool IsTransient()
+		{
+			return Id == 0;
+		}
+	}
+}
