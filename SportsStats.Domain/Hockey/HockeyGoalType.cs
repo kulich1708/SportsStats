e5eb3c529d8f8bfa986f70d79833a6f5baaf7f5d
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-02 12:15:22 +0300

    feat(domain): add HockeyGoalType with description

---
+++ SportsStats.Domain/Hockey/HockeyGoalType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Hockey
+{
+	public enum HockeyGoalStrengthType
+	{
+		EvenStrength,
+		PowerPlay,
+		Shorthanded,
+	}
+	public enum HockeyGoalNetType
+	{
+		EmptyNet
+	}
+
+	public static class HockeyGoalTypeText
+	{
+		private static readonly Dictionary<HockeyGoalStrengthType, string> StrengthDescription = new()
+		{
+			[HockeyGoalStrengthType.EvenStrength] = "В равных составах",
+			[HockeyGoalStrengthType.PowerPlay] = "В большинстве",
+			[HockeyGoalStrengthType.Shorthanded] = "В меньшенстве",
+		};
+		public static string GetDescription(this HockeyGoalStrengthType strengthType)
+		{
+			return StrengthDescription.TryGetValue(strengthType, out var text) ? text : string.Empty;
+		}
+		public static Dictionary<HockeyGoalNetType, string> NetDescription = new()
+		{
+			[HockeyGoalNetType.EmptyNet] = "В пустые ворота"
+		};
+		public static string GetDescription(this HockeyGoalNetType netType)
+		{
+			return NetDescription.TryGetValue(netType, out var text) ? text : string.Empty;
+		}
+
+	}
+}
