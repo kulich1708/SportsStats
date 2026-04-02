e3202c9fad4a163b675a6b7b58620b51c063f80b
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-02 15:50:56 +0300

    feat(domain): add PlayerPosition<T> value object and Player<T> class

---
+++ SportsStats.Domain/Hockey/HockeyPositionsType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Hockey
+{
+	internal enum HockeyPositionsType
+	{
+		LeftWinger,
+		RightWinger,
+		Center,
+		LeftDefenseman,
+		RightDefenseman,
+		Goalie
+	}
+}
