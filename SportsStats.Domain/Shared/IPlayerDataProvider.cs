29f974a6b690aa236e33fa08dcbc08490b2225e6
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-04 12:47:12 +0300

    feat(providers): add IExistenceChecker, IPlayerDataProvider, ITournamentDataProvider

---
+++ SportsStats.Domain/Shared/IPlayerDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Shared
+{
+	public interface IPlayerDataProvider
+	{
+		public int GetTeamId(int playerId);
+	}
+}
