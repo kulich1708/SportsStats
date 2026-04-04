c5c8620d7985cdecfae0a2f48ef3e6836bfa2fcd
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-04 21:45:40 +0300

    feat(domain): add MatchRosterRules and TournamentRules

---
+++ SportsStats.Domain/Tournaments/Rules/TournamentRules.cs
@@ -0,0 +1,20 @@
+﻿using SportsStats.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Tournaments.Rules
+{
+	public class TournamentRules : BaseEntity
+	{
+
+		public int MatchDurationRulesId { get; private set; }
+		public int MatchRosterRulesId { get; private set; }
+
+		public TournamentRules(int matchDurationRulesId, int matchRosterRulesId)
+		{
+			MatchDurationRulesId = matchDurationRulesId;
+			MatchRosterRulesId = matchRosterRulesId;
+		}
+	}
+}
