268123339f4e25194eb949e5a471d1ce6b55c241
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-04 12:47:12 +0300

    feat(providers): add IExistenceChecker, IPlayerDataProvider, ITournamentDataProvider

---
+++ SportsStats.Domain/Shared/IExistenceChecker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Shared
+{
+	internal interface IExistenceChecker
+	{
+		public bool Exists(int matchId);
+	}
+}
---
+++ SportsStats.Domain/Shared/ITournamentDataProvider.cs
@@ -0,0 +1,12 @@
+﻿using SportsStats.Domain.Tournaments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Shared
+{
+	public interface ITournamentDataProvider
+	{
+		public TimeBasedTournamentRules GetTimeBasedTournamentRules(int tournamentId);
+	}
+}
