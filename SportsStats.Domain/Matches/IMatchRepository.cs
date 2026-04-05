af3a7eade12b0d22bdeec36ca85e8045e7a5a041
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-05 18:50:34 +0300

    feat(domain): add IMatchRepository, IPlayerRepository, ITeamRepository, ITournamentRepository and ITournamentRegistrationRepository

---
+++ SportsStats.Domain/Matches/IMatchRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Matches
+{
+	public interface IMatchRepository
+	{
+		public Match FindById(int id);
+		public Match Save(Match match);
+	}
+}
