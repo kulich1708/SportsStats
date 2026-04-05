74ae483d77dbaa1c054ce1b6d66af57f95f9503a
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-05 17:26:38 +0300

    feat(domain): add MatchWinType

---
+++ SportsStats.Domain/Shared/Enums/MatchWinType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Shared.Enums
+{
+	public enum MatchWinType
+	{
+		REGULATION_WIN,
+		OT_WIN,
+		REGULATION_LOSS,
+		OT_LOSS,
+		DRAW
+	}
+}
