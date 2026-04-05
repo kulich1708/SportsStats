7b90b11017c8d0b638b33448779240fba312f725
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-05 17:27:53 +0300

    feat(domain): add Teams and Tournaments

--- SportsStats.Domain/Tournaments/Tournament.cs
+++ SportsStats.Domain/Tournaments/Tournament.cs
@@ -1,11 +1,17 @@
 ﻿using SportsStats.Domain.Common;
+using SportsStats.Domain.Tournaments.Rules;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SportsStats.Domain.Tournaments
 {
-	public class Tournament : IAggregateRoot
+	public class Tournament : BaseEntity, IAggregateRoot
 	{
+		private TournamentRules _tournamentRules;
+		public string Name { get; private set; }
+		public DateTime StartedAt { get; private set; }
+		public DateTime FinishedAt { get; private set; }
+
 	}
 }
