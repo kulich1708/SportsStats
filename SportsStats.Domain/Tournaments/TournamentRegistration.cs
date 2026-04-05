f129aeb4b30b0f0c4128a0c3bc032c952e2c0836
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-05 18:46:10 +0300

    feat(domain): add TournamentRegistration

---
+++ SportsStats.Domain/Tournaments/TournamentRegistration.cs
@@ -0,0 +1,9 @@
+﻿using SportsStats.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Tournaments
+{
+	public record TournamentRegistration(int TournamentId, int TeamId);
+}
