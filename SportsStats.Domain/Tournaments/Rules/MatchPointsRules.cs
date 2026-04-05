bb33915a3b904c7b61cd3a8811e5deb20965a625
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-05 17:26:20 +0300

    feat(domain): add MatchPointsRules and add draw setting

---
+++ SportsStats.Domain/Tournaments/Rules/MatchPointsRules.cs
@@ -0,0 +1,19 @@
+﻿using SportsStats.Domain.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Tournaments.Rules
+{
+	public class MatchPointsRules
+	{
+		public Dictionary<MatchWinType, int> Points = new();
+		public MatchPointsRules(int regWin, int otWin, int regLoss, int otLoss)
+		{
+			Points.Add(MatchWinType.REGULATION_WIN, regWin);
+			Points.Add(MatchWinType.OT_WIN, otWin);
+			Points.Add(MatchWinType.REGULATION_LOSS, regLoss);
+			Points.Add(MatchWinType.OT_LOSS, otLoss);
+		}
+	}
+}
