78b217588f0d4ea39111fc9cda0a6bb8d7141464
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-05 18:45:28 +0300

    feat(domain): add TournamentStatus

--- SportsStats.Domain/Tournaments/Tournament.cs
+++ SportsStats.Domain/Tournaments/Tournament.cs
@@ -8,10 +8,11 @@ namespace SportsStats.Domain.Tournaments
 {
 	public class Tournament : BaseEntity, IAggregateRoot
 	{
-		private TournamentRules _tournamentRules;
 		public string Name { get; private set; }
 		public DateTime StartedAt { get; private set; }
 		public DateTime FinishedAt { get; private set; }
+		public TournamentStatus Status { get; private set; }
+		public TournamentRules TournamentRules { get; private set; }
 
 	}
 }
