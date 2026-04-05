bee401b3c07b4650fe224da2f593a63ee0e21463
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-05 21:52:35 +0300

    feat(domain): add IGoalRepository

---
+++ SportsStats.Domain/Matches/Goals/IGoalRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Matches.Goals
+{
+	public interface IGoalRepository
+	{
+		public GoalEvent FindById(int goalId);
+		public GoalEvent Save(GoalEvent goalEvent);
+	}
+}
