e69738c03618a58af66462fe41903bee71af4826
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-06 15:23:28 +0300

    feat(application): add PlayerApplicationService

---
+++ SportsStats.Application/Players/PlayerApplicationService.cs
@@ -0,0 +1,49 @@
+﻿using SportsStats.Domain.Players;
+using SportsStats.Domain.Teams;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Application.Players
+{
+	public class PlayerApplicationService
+	{
+		private IPlayerRepository _playerRepository;
+		private ITeamRepository _teamRepository;
+
+		public Player Create(string name, string surname)
+		{
+			Player player = new Player(name, surname);
+			return _playerRepository.Save(player);
+		}
+		public void ChangeTeam(int playerId, int teamId)
+		{
+			Player player = GetPlayerOrThrow(playerId);
+			Team team = GetTeamOrThrow(teamId);
+
+			player.ChangeTeam(teamId);
+
+			_playerRepository.Save(player);
+		}
+
+		public void SetPosition(int playerId, PositionType position)
+		{
+			Player player = GetPlayerOrThrow(playerId);
+
+			player.SetPosition(position);
+
+			_playerRepository.Save(player);
+		}
+
+		private Player GetPlayerOrThrow(int playerId)
+		{
+			return _playerRepository.FindById(playerId)
+				?? throw new ArgumentException("Игрок с таким Id не найден");
+		}
+		private Team GetTeamOrThrow(int teamId)
+		{
+			return _teamRepository.FindById(teamId)
+				?? throw new ArgumentException("Команда с таким id не найдена");
+		}
+	}
+}
