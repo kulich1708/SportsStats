39fe9353dc6a6c0a79581866808c7ea3da6e4be9
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-06 13:18:08 +0300

    feat(domain): add MatchCreationService

---
+++ SportsStats.Domain/Services/IMatchCreationService.cs
@@ -0,0 +1,12 @@
+﻿using SportsStats.Domain.Matches;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Services
+{
+	public interface IMatchCreationService
+	{
+		public Match CreateMatch(int tournamentId, int homeTeamId, int awayTeamId);
+	}
+}
