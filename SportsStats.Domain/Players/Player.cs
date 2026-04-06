1f29f3fe559f6fad71a91a869d980bb27aa18041
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-06 15:11:25 +0300

    refactor(domain): delete Birthday and JerseyNumber in Player

--- SportsStats.Domain/Players/Player.cs
+++ SportsStats.Domain/Players/Player.cs
@@ -9,10 +9,8 @@ namespace SportsStats.Domain.Players
 	{
 		public string Name { get; private set; }
 		public string Surname { get; private set; }
-		public DateOnly Birthday { get; private set; }
 		public int TeamId { get; private set; }
 		public PositionType Position { get; private set; }
-		public int JerseyNumber { get; private set; }
 		public Player(string name, string surname)
 		{
 			ValidateNameAndSurname(name, surname);
@@ -30,10 +28,6 @@ namespace SportsStats.Domain.Players
 			if (surname.Count() > 20)
 				throw new ArgumentException("Фамилия не может быть длинее 20 символов");
 		}
-		public void SetBirthday(DateOnly birthday)
-		{
-			Birthday = birthday;
-		}
 		public void ChangeTeam(int teamId)
 		{
 			TeamId = teamId;
@@ -41,13 +35,6 @@ namespace SportsStats.Domain.Players
 		public void SetPosition(PositionType position)
 		{
 			Position = position;
-		}
-		public void SetJerseyNumber(int jerseyNumber)
-		{
-			if (jerseyNumber < 1 || jerseyNumber > 99)
-				throw new ArgumentException("Номер игрока должен быть от 1 до 99");
-
-			JerseyNumber = jerseyNumber;
 		}
 	}
 }
