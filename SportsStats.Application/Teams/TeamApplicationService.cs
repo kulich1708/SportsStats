44b9d27e37e5f8489d3afadf99a8eab8f8e25f0b
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-06 10:15:55 +0300

    feat(application): add TeamApplicationService

---
+++ SportsStats.Application/Teams/TeamApplicationService.cs
@@ -0,0 +1,26 @@
+﻿using SportsStats.Domain.Shared;
+using SportsStats.Domain.Teams;
+using SportsStats.Domain.Tournaments;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SportsStats.Application.Teams
+{
+	public class TeamApplicationService
+	{
+		private ITeamRepository _teamRepository;
+		public TeamApplicationService(ITeamRepository teamRepository)
+		{
+			_teamRepository = teamRepository;
+		}
+
+		public Team Create(int teamId, string name)
+		{
+			Team team = new Team(name);
+			return _teamRepository.Save(team);
+		}
+
+	}
+}
