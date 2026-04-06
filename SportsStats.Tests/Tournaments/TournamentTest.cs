5a24cdafbc7341a67c28a92d9a2e2362cedc12c6
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-06 22:46:05 +0300

    fix(domain): add missing business rule for SetAssists

---
+++ SportsStats.Tests/Tournaments/TournamentTest.cs
@@ -0,0 +1,12 @@
+﻿using SportsStats.Domain.Tournaments;
+using SportsStats.Domain.Tournaments.Rules;
+using System;
+using Xunit;
+
+namespace SportsStats.Tests.Tournaments
+{
+	public class TournamentSetStatusTests
+	{
+
+	}
+}
