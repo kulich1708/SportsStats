bf8a94c45d8bacf41aa7ac434d82c93de1e6a9de
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-07 11:53:07 +0300

    feat: add EF Core with initial table

---
+++ SportsStats.Infrastructure/Persistence/DbContexts/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+
+//namespace SportsStats.Infrastructure.Persistence.DbContexts
+//{
+//	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+//	{
+//		public AppDbContext CreateDbContext(string[] args)
+//		{
+//			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+//			// Строка подключения для миграций
+//			optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=SportsStats;Username=VladislavKulichkov;Password=qw06062013?");
+
+//			return new AppDbContext(optionsBuilder.Options);
+//		}
+//	}
+//}
