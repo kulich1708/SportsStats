f4fce9837604005241c98cde355921d247d35769
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-07 18:11:08 +0300

    refactor(domain): Redesigned the scoring of teams

--- SportsStats.Domain/Tournaments/Rules/MatchPointsRules.cs
+++ SportsStats.Domain/Tournaments/Rules/MatchPointsRules.cs
@@ -1,26 +1,34 @@
 ﻿using SportsStats.Domain.Shared.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace SportsStats.Domain.Tournaments.Rules
+public record MatchPointsRules
 {
-	public record MatchPointsRules
-	{
-		public Dictionary<MatchWinType, int> Points = new();
-		public MatchPointsRules(int regWin, int otWin, int regLoss, int otLoss, int? draw = null)
-		{
-			Points.Add(MatchWinType.REGULATION_WIN, regWin);
-			Points.Add(MatchWinType.OT_WIN, otWin);
-			Points.Add(MatchWinType.REGULATION_LOSS, regLoss);
-			Points.Add(MatchWinType.OT_LOSS, otLoss);
-			if (draw.HasValue)
-				Points.Add(MatchWinType.DRAW, draw.Value);
-		}
+	public int WinPoints { get; private set; }      // Победа в основное время
+	public int OTWinPoints { get; private set; }    // Победа в овертайме
+	public int LossPoints { get; private set; }     // Поражение в основное время
+	public int OTLossPoints { get; private set; }   // Поражение в овертайме
+	public int? DrawPoints { get; private set; }    // Ничья (если есть)
 
-		public static MatchPointsRules CreateKHLRules()
-		{
-			return new MatchPointsRules(2, 2, 0, 1);
-		}
+	private MatchPointsRules() { }
+	public MatchPointsRules(int win, int otWin, int loss, int otLoss, int? draw = null)
+	{
+		WinPoints = win;
+		OTWinPoints = otWin;
+		LossPoints = loss;
+		OTLossPoints = otLoss;
+		DrawPoints = draw;
+	}
+
+	public int GetPoints(MatchWinType outcome) => outcome switch
+	{
+		MatchWinType.REGULATION_WIN => WinPoints,
+		MatchWinType.OT_WIN => OTWinPoints,
+		MatchWinType.REGULATION_LOSS => LossPoints,
+		MatchWinType.OT_LOSS => OTLossPoints,
+		MatchWinType.DRAW => DrawPoints ?? 0,
+		_ => 0
+	};
+	public static MatchPointsRules CreateKHLRules()
+	{
+		return new MatchPointsRules(win: 2, otWin: 2, loss: 0, otLoss: 1);
 	}
 }
