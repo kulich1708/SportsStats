3e8f80443d7cf7c1e19287f56db9637c0f55ba0a
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-07 11:53:07 +0300

    feat: add EF Core with initial table

---
+++ SportsStats.Infrastructure/Persistence/DbContexts/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SportsStats.Domain.Players;
+
+namespace SportsStats.Infrastructure.Persistence.DbContexts
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options)
+		{
+		}
+
+		// Это свойство = таблица Products в базе данных
+		public DbSet<Player> Products { get; set; }
+	}
+}
