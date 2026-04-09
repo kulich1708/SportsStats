44e2572a2a9ca535b1ec86c53218fc8ee9e71451
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-09 22:38:44 +0300

    feat(API): add TeamsController with all endpoints

---
+++ SportsStats.API/Controllers/TeamsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsStats.Application.Teams;
+using SportsStats.Application.Teams.DTOs.Requests;
+using SportsStats.Application.Teams.DTOs.Responses;
+using SportsStats.Domain.Teams;
+using SportsStats.Infrastructure.Persistence.DbContexts;
+
+namespace SportsStats.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TeamsController(TeamApplicationService teamApplicationService) : ControllerBase
+	{
+		private readonly TeamApplicationService _teamApplicationService = teamApplicationService;
+
+		// GET: api/Teams
+		[HttpGet]
+		public async Task<ActionResult<List<TeamDTO>>> GetAll([FromQuery] int? tournamentId = null)
+		{
+			return Ok(await _teamApplicationService.GetAllAsync(tournamentId));
+		}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<TeamDTO>> Get(int id)
+		{
+			return Ok(await _teamApplicationService.GetAsync(id));
+		}
+		[HttpPost]
+		public async Task<ActionResult<int>> Create([FromBody] CreateTeamDTO dto)
+		{
+			string name = dto.Name;
+			if (string.IsNullOrWhiteSpace(name))
+				return BadRequest(new { error = "Имя команды не должно быть пустым" });
+
+			return Ok(await _teamApplicationService.CreateAsync(name));
+		}
+	}
+}
