421835fe1b739d920321d55073dc58e4e105e0bf
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-09 17:32:12 +0300

    feat(infrastructure): add SystemTimeProvider

---
+++ SportsStats.Infrastructure/Services/SystemTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using SportsStats.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Infrastructure.Services
+{
+	public class SystemTimeProvider : ITimeProvider
+	{
+		public DateTime GetCurrentTime()
+		{
+			return DateTime.UtcNow;
+		}
+	}
+}
