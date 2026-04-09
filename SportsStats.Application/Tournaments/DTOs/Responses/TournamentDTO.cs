4f84b647ec4f374135d3981fed4edac292be9c1a
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-09 16:51:15 +0300

    feat(tournaments): add query methods and DTOs
    - Add GetTournamentsAsync with filtering by started status
    - Add GetTournamentAsync by id
    - Create TournamentDTO and CreateTournamentRequest DTOs
    - Implement TournamentMapper

---
+++ SportsStats.Application/Tournaments/DTOs/Responses/TournamentDTO.cs
@@ -0,0 +1,18 @@
+﻿using SportsStats.Domain.Tournaments;
+using SportsStats.Domain.Tournaments.Rules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Application.Tournaments.DTOs.Responses
+{
+	public record TournamentDTO
+	(
+		string Name,
+		DateTime StartedAt,
+		DateTime FinishedAt,
+		TournamentStatus Status,
+		TournamentRules TournamentRules,
+		IReadOnlyList<int> TeamsId
+	);
+}
---
+++ SportsStats.Application/Tournaments/TournamentMapper.cs
@@ -0,0 +1,20 @@
+﻿using SportsStats.Application.Tournaments.DTOs.Requests;
+using SportsStats.Application.Tournaments.DTOs.Responses;
+using SportsStats.Domain.Tournaments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Application.Tournaments
+{
+	public static class TournamentMapper
+	{
+		public static TournamentDTO ToDTO(Tournament tournament) => new(
+				tournament.Name,
+				tournament.StartedAt,
+				tournament.FinishedAt,
+				tournament.Status,
+				tournament.TournamentRules,
+				tournament.TeamsId);
+	}
+}
