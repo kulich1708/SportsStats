1b04f7ffb8940b0b816f4731d3f66ce6c6e5dbed
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-10 10:40:24 +0300

    feat(application): working through the matchService in the MatchApplicationService and adding DI to the Program. Add method in PlayerRepository

--- SportsStats.Infrastructure/Persistence/Repositories/PlayerRepository.cs
+++ SportsStats.Infrastructure/Persistence/Repositories/PlayerRepository.cs
@@ -34,5 +34,9 @@ namespace SportsStats.Infrastructure.Persistence.Repositories
 			Team team = await _context.Teams.FirstOrDefaultAsync(t => t.Id == teamId);
 			return team == null ? [] : await _context.Players.Where(p => p.TeamId == teamId).ToListAsync();
 		}
+		public async Task<List<Player>> GetAsync(List<int> playersId)
+		{
+			return await _context.Players.Where(p => playersId.Contains(p.Id)).ToListAsync();
+		}
 	}
 }
