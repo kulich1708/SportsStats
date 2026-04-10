b802141dd1b11c24e6b538b52912983d19ab4b05
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-10 10:33:45 +0300

    refactor(domain): rename MatchCreationService to MatchService and add Start method with validation roster

--- SportsStats.Domain/Services/IMatchService.cs
+++ SportsStats.Domain/Services/IMatchService.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using SportsStats.Domain.Tournaments;
+using SportsStats.Domain.Players;
+using SportsStats.Domain.Teams;
 
 namespace SportsStats.Domain.Services
 {
-	public interface IMatchCreationService
+	public interface IMatchService
 	{
 		public Match CreateMatch(Tournament tournament, int homeTeamId, int awayTeamId);
+		public void Start(Match match, List<Player> homeTeamRoster, List<Player> awayTeamRoster, Team homeTeam, Team awayTeam, DateTime startedAt);
 	}
 }
