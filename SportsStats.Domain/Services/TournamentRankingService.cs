e691c3ce76889ae7f8ae68dc152d13b36bccb824
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-15 18:22:47 +0300

    feat: add team statistics

---
+++ SportsStats.Domain/Services/TournamentRankingService.cs
@@ -0,0 +1,19 @@
+﻿using SportsStats.Domain.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Services
+{
+	public class TournamentRankingService
+	{
+		public List<TeamStats> RankingByPoints(List<TeamStats> teamsStats)
+		{
+			return teamsStats.OrderBy(stat => stat.Points)
+							 .ThenBy(stat => stat.RegularWins)
+							 .ThenBy(stat => stat.OTWins)
+							 .ThenBy(stat => stat.Draws)
+							 .ToList();
+		}
+	}
+}
