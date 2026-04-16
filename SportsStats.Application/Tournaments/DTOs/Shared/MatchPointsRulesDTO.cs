6b91701446485547c2c67bfe6c2b5105ee0f2102
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-16 15:49:16 +0300

    feat: add all rules DTOs

---
+++ SportsStats.Application/Tournaments/DTOs/Shared/MatchPointsRulesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Application.Tournaments.DTOs.Shared
+{
+	public record MatchPointsRulesDTO(
+		int WinPoints,
+		int OtWinPoints,
+		int LossPoints,
+		int OtLossPoints,
+		int? DrawPoints
+	);
+}
