2eba8d798224e51242b63db46f38d44128dcba5c
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-17 23:13:59 +0300

    feat: add data generator. Generator tournaments, teams, players, matches, goals

---
+++ ConsoleApp/INamesData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+	public interface INamesData
+	{
+		public List<string> FirstNames { get; }
+		public List<string> LastNames { get; }
+	}
+}
