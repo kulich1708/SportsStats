a6fb1e30a5f7545d17b7ead45d677cc1c2ce662c
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-04-20 12:29:56 +0300

    feat(infrastructure): add migratinons with PhotoMime

---
+++ SportsStats.Infrastructure/Migrations/20260420092242_AddPhotoMime.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsStats.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoMime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoMime",
+                table: "Tournaments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoMime",
+                table: "Teams",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Citizenship_PhotoMime",
+                table: "Players",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoMime",
+                table: "Players",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoMime",
+                table: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoMime",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "Citizenship_PhotoMime",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoMime",
+                table: "Players");
+        }
+    }
+}
