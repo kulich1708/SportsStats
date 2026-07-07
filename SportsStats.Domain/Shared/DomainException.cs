56fa862a24f2484cb58741eabc310af1c4b2ea07
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-07-07 15:07:34 +0300

    feat: add DomainException class

---
+++ SportsStats.Domain/Shared/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Shared
+{
+	public class DomainException : Exception
+	{
+		public DomainException(string message) : base(message) { }
+		public DomainException(string message, params string[] args) : base(string.Format(message, args)) { }
+		public DomainException(string message, params DateTime[] args) : base(string.Format(message, args)) { }
+	}
+}
---
+++ SportsStats.Domain/Shared/Error.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Domain.Shared
+{
+	public static partial class Error
+	{
+	}
+}
