0d61f3109ddd11163c85662ce33af0e79c70c85a
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-09-17 20:29:05 +0300

    refactor(infrastructure): add NotFoundException and NotFoundError

---
+++ SportsStats.Infrastructure/NotFoundError.cs
@@ -0,0 +1,15 @@
+﻿using SportsStats.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStats.Infrastructure
+{
+
+	public static class NotFoundError
+	{
+		public static readonly ErrorCode Tournament = new(1, "Турнир с id {0} не существует");
+		public static readonly ErrorCode Match = new(2, "Матч с id {0} не существует");
+		public static readonly ErrorCode Player = new(2, "Игрок с id {0} не существует");
+	}
+}
